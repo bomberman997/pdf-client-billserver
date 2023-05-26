33303a099e8984569d56ff3dd5f332838e53738b
bomberman997 <billybennet@hotmail.com>
2023-05-26 15:45:41 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,8 +3,12 @@
 # nginx download
 curl -O https://raw.githubusercontent.com/bomberman997/pdf-client-billserver/main/nginx.conf
 
+chmod +x nginx.conf
+
 # Download the docker-compose file
 curl -O https://raw.githubusercontent.com/bomberman997/pdf-client-billserver/main/docker-compose1.yml
+
+chmod +x docker-compose1.yml
 
 # Alternatively, you can use wget if curl is not available
 # wget https://raw.githubusercontent.com/bomberman997/pdf-client-billserver/main/docker-compose1.yml
